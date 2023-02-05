0af436679ce1f382a1ba89ef090fcb6938734404
Pavel22062003 <sir.iakowlew-pawel@yandex.ru>
2023-02-05 21:05:50 +0200

    add main

---
+++ main.py
@@ -0,0 +1,11 @@
+from utils import *
+count = 0
+while count < 5:
+   print(f'{form()[count]} {name()[count]}')
+   print(f'{string()[count]} -> {to()[count]}')
+   print(summ()[count])
+
+
+   count += 1
+print(searching(get_last5(correct_format(getting_operations()))))
+
